5d5acf380e1ab626c911b8a7cf25b3fb47d1ac80
敏盛 李 <lms99251@hotmail.com>
2019-03-06 16:09:50 +0800

    NLog在.net core上的使用

--- NLogInDotnetCoreConlose/Program.cs
+++ NLogInDotnetCoreConlose/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
 
 namespace NLogInDotnetCoreConlose
 {
@@ -6,7 +10,35 @@ namespace NLogInDotnetCoreConlose
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var servicesProvider = BuildDi();
+            var runner = servicesProvider.GetRequiredService<Runner>();
+            runner.DoAction("runner Action");
+
+            var runner2 = servicesProvider.GetRequiredService<Runner2>();
+            runner.DoAction("runner2 Action");
+
+            Console.WriteLine("Press ANY key to exit");
+            Console.ReadLine();
+
+            // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            NLog.LogManager.Shutdown();
+        }
+
+        private static ServiceProvider BuildDi()
+        {            
+
+            return new ServiceCollection()
+                .AddLogging(builder => {
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                    builder.AddNLog(new NLogProviderOptions
+                    {
+                        CaptureMessageTemplates = true,
+                        CaptureMessageProperties = true
+                    });
+                })
+                .AddTransient<Runner>()
+                .AddTransient<Runner2>()
+                .BuildServiceProvider();
         }
     }
 }
---
+++ NLogInDotnetCoreConlose/Runner2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NLogInDotnetCoreConlose
+{
+    public class Runner2 
+    {
+        private readonly ILogger<Runner2> _logger;
+
+        public Runner2(ILogger<Runner2> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            _logger.LogDebug(20, "Doing hard work! {Action}", name);
+        }
+    }
+}
